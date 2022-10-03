901d8193364b8b9ff6eee00c9d275396315316f9
Luiz Afonso Silva Costa <luizcosta12322@yahoo.com>
2022-10-03 11:38:57 -0300

    Add project

---
+++ tempo-estudo/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "./components/buttom/Button";
+import Formulario from "./components/formulario/Formulario";
+import Lista from "./components/lista/Lista";
+
+function App() {
+  return (
+    <div>
+      <Formulario />
+
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
---
+++ tempo-estudo/src/components/formulario/Formulario.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Button from "../buttom/Button";
+
+class Formulario extends Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="tarefa">
+            Adicione um novo estudo
+          </label>
+
+          <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar ?" required/>
+        </div>
+
+        <div>
+          <label htmlFor="tempo">
+            Tempo
+          </label>
+
+          <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00"/>
+        </div>
+
+        <Button />
+      </form>
+    );
+  }
+}
+
+export default Formulario;
